dba0d8b81bb592f8fe3a1c6d21915385f582fbe4
John Doe <tbousiou@gmail.com>
2025-04-25 18:00:52 +0300

    minor changes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,17 +10,17 @@ export default function MovieCard({ movie: { title, vote_average, poster_path, o
                 <div className="content">
                     <div className="rating">
                         <img src="star.svg" alt="Star icon" />
-                        <p>{vote_average ? vote_average.toFixed(1): 'N/A'}</p>
+                        <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
                         <span>•</span>
                         <p className="lang">{original_language}</p>
                         <span>•</span>
                         <p className="year">
-                            {release_date ? release_date.split('-')[0] : 'N/A'} 
+                            {release_date ? release_date.split('-')[0] : 'N/A'}
                         </p>
                     </div>
                 </div>
             </div>
-            
+
         </div>
     )
 }
