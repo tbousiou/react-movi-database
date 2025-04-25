5e7286d4efaf894b9b96372848c160dcce8f7839
John Doe <tbousiou@gmail.com>
2025-04-25 17:58:57 +0300

    Movie card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Search from './components/Search'
 import Spinner from './components/Spinner'
+import MovieCard from './components/MovieCard'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -25,18 +26,18 @@ function App() {
   const fetchMovies = async () => {
     setLoading(true);
     setErrorMessage(null);
-    
+
     try {
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
-      
+
       if (!response.ok) {
         throw new Error('Failed to fetch movies');
       }
-      
+
       const data = await response.json();
       console.log(data);
-      
+
       if (data.results.length === 0) {
         setErrorMessage('No movies found.');
         setMovieList([]);
@@ -49,13 +50,13 @@ function App() {
       setErrorMessage('Failed to fetch movies. Please try again later.');
     } finally {
       setLoading(false);
-    } 
+    }
   }
 
   useEffect(() => {
     fetchMovies();
   }
-  , []);
+    , []);
 
   return (
     <main>
@@ -66,29 +67,25 @@ function App() {
           <h1>Find <span className='text-gradient'>Movies</span> You Enjoy Without the Hassle</h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-        <section className='all-movies'>
-          <h2 className='mt-[40px]'>All Movies</h2>
+        <section className="all-movies">
+          <h2>All Movies</h2>
 
           {loading ? (
             <Spinner />
           ) : errorMessage ? (
-              <p className='text-red-500'>{errorMessage}</p>
-            ) : (
-              movieList.map((movie) => (
-                <div className='movie-card' key={movie.id}>
-                  <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                  <h2>{movie.title}</h2>
-                  <p>{movie.release_date}</p>
-                  <p>{movie.overview}</p>
-                </div>
-              ))
-            )}
-
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
         </section>
-        
-        
+
+
       </div>
-     </main>
+    </main>
 
   )
 }
